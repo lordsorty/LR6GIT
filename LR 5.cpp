9d079e5694ca00a38739de09e0cd098f8e35f9ef
mbukator <maxbukator03@gmail.com>
2021-10-29 04:53:58 +0300

    added comments

--- LR 5.cpp
+++ LR 5.cpp
@@ -9,10 +9,10 @@ int main()
     cin >> y;
     switch (y)
     {
-    case 1: case 2: case 12: cout << "\n" "Winter" "\n"; break;
-    case 3: case 4: case 5: cout << "\n" "Spring" "\n"; break;
-    case 6: case 7: case 8: cout << "\n" "Summer" "\n"; break;
-    case 9: case 10: case 11: cout << "\n" "Autumn" "\n"; break;
+    case 1: case 2: case 12: cout << "\n" "Winter" "\n"; break; //якщо 1,2,12 вивести Winter
+    case 3: case 4: case 5: cout << "\n" "Spring" "\n"; break; //якщо 3,4,5 вивести Spring
+    case 6: case 7: case 8: cout << "\n" "Summer" "\n"; break; //якщо 6,7,8 вивести Summer
+    case 9: case 10: case 11: cout << "\n" "Autumn" "\n"; break; //якщо 9,10,11 вивести Autumn
     default: cout << "Error";
     }
 }
