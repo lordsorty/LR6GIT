5970d80d2f1d365feffd3b4a93bfaa5c2ba5e054
mbukator <maxbukator03@gmail.com>
2021-11-11 06:08:56 +0200

    added one more new file

---
+++ LR7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std;
+int main()
+{
+    int k;
+    float x, Y, sum;
+    cout << "Input x: ";
+    cin >> x;
+    sum = 0;
+    for (k = 1; k < 11; k++)
+    {
+        sum = sum + k;
+    }
+    Y = (sum * sin(k * x) + 2 * x) / ((x * x) + 5);
+    cout << "Y= " << setprecision(2) << Y << endl;
+}
